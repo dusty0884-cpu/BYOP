af717abf8b4a2d7cc42b6a1bbf7ceae61a4b739c
dusty0884-cpu <270168119+dusty0884-cpu@users.noreply.github.com>
2026-05-07 07:32:08 -0400

    Update vercel.ts

--- vercel.ts
+++ vercel.ts
@@ -1,1 +1,27 @@
-Vercel CLI command: npx vercel inspect dpl_BfS1sv2YRWu7HF7MKqzJC3A5mu8E --logs
+
+import { type VercelConfig } from '@vercel/config/v1';
+
+// Example: Programmatic Config
+export const config: VercelConfig = {
+  // Use Vercel to build your app
+  buildCommand: 'npm run build',
+  
+  // Custom headers
+  headers: [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'X-Frame-Options',
+          value: 'DENY',
+        },
+      ],
+    },
+  ],
+
+  // Rewrites for API or Single Page Apps
+  rewrites: [
+    { source: '/api/:match*', destination: '/api/:match*' },
+    { source: '/(.*)', destination: '/index.html' },
+  ],
+};
