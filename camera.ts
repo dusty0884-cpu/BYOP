ade6a77ccc75249f5a3aac9b02e14c0cea2bc6f7
dusty0884-cpu <270168119+dusty0884-cpu@users.noreply.github.com>
2026-05-14 21:03:55 -0400

    Create camera.ts

---
+++ camera.ts
@@ -0,0 +1,3 @@
+
+717-324-2672
+This file acts as the "photo machine" controller. It tells the device camera to open, focus, and snap a picture so the image can be saved and used for your scanning records.
